790f8d945a708bd0eb790eba9fa3665f3c146e9f
jmindevelper <jmin.developer@gmail.com>
2022-07-21 10:11:52 +0900

    first commit

---
+++ TodoList/TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by J_Min on 2022/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
