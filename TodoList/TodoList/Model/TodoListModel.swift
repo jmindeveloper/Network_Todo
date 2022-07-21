8a3d2c83f95da6d9bf79340c0a38531f9b6a0016
jmindevelper <jmin.developer@gmail.com>
2022-07-21 11:50:04 +0900

    [feat] TodoListModel 정의
    - id, title, createDate

---
+++ TodoList/TodoList/Model/TodoListModel.swift
@@ -0,0 +1,14 @@
+//
+//  TodoListModel.swift
+//  TodoList
+//
+//  Created by J_Min on 2022/07/21.
+//
+
+import Foundation
+
+struct TodoListModel: Codable {
+    let id: String
+    let title: String
+    let crateDate: String
+}
